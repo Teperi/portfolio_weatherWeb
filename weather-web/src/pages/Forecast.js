08831772aa24367f03d9a5da8a34cfb5bf236d33
Teperi Lee <teperi31@gmail.com>
2019-05-01 20:23:47 +0900

    일기예보 상세보기 뷰 제작중

--- weather-web/src/pages/Forecast.js
+++ weather-web/src/pages/Forecast.js
@@ -1,36 +1,47 @@
 import React, { Component } from 'react';
-import { FaBeer } from 'react-icons/fa';
+import { FaBeer, FaCircle } from 'react-icons/fa';
 import { IconContext } from 'react-icons';
-import { WiCelsius } from 'react-icons/wi';
+import { WiSnow, WiCelsius } from 'react-icons/wi';
+import { TiMinus } from 'react-icons/ti';
 import './Forecast.scss';
 
 export default class Forecast extends Component {
   render() {
     return (
-      <div>
-        여기에 상세 내용을 담을 예정입니다.
-        <h3>
+      <div className='timeline'>
+        {/* <h3>
           {' '}
           Lets go for a <FaBeer />?{' '}
         </h3>
         <IconContext.Provider value={{ size: '5em' }}>
           <WiCelsius />
-        </IconContext.Provider>
-        // Card test
+        </IconContext.Provider> */}
         <div className='card'>
           <div className='card_left'>
-            <img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/343086/h8fnwL1.png' />
+            <IconContext.Provider value={{ size: '3.5em' }}>
+              <WiSnow />
+            </IconContext.Provider>
+          </div>
+          <div className='card_center'>
+            <h1>Right Now</h1>
+            <p>Light Snow</p>
           </div>
           <div className='card_right'>
-            <h1>KILL BILL: VOL. 1</h1>
-            <div className='card_right__details'>
-              <ul>
-                <li>2003</li>
-                <li>111 min</li>
-                <li>Action</li>
-              </ul>
-            </div>
+            <p>
+              1
+              <IconContext.Provider value={{ className: 'celsiusicon' }}>
+                <WiCelsius />
+              </IconContext.Provider>
+            </p>
           </div>
+          <IconContext.Provider value={{ color: '#fdfdfd', className: 'card_bottomcircle' }}>
+            <FaCircle />
+          </IconContext.Provider>
+        </div>
+        <div className='nonCard'>
+          <IconContext.Provider value={{ color: '#fdfdfd', size: '2.5em', className: 'line' }}>
+            <TiMinus />
+          </IconContext.Provider>
         </div>
       </div>
     );
