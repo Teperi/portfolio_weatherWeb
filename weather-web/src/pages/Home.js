7319997c805c1dbbb30a8dfa21777d21fb4088a2
Teperi Lee <teperi31@gmail.com>
2019-05-03 13:52:24 +0900

    발표자료 정리
    개인 key 삭제
    기상청 및 네이버 연결 보강

--- weather-web/src/pages/Home.js
+++ weather-web/src/pages/Home.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './Home.scss';
 
-
-
-const Home = () => {
-  return (
-    <div>
-      <p>여기에 날씨 목록을 띄울 예정입니다.</p>
-    </div>
-  );
-};
-
-export default Home;
+export default class Home extends Component {
+  render() {
+    return (
+      <div className='list'>
+        <div className='list_card'>카드 디자인이 들어올 예정</div>
+        <div className='list_card'>카드 디자인이 들어올 예정</div>
+        <div className='list_card'>카드 디자인이 들어올 예정</div>
+        <div className='list_card'>카드 디자인이 들어올 예정</div>
+        <div className='list_card'>카드 디자인이 들어올 예정</div>
+        <div className='list_card'>카드 디자인이 들어올 예정</div>
+      </div>
+    );
+  }
+}
