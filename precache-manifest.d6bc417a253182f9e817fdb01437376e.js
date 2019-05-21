38ce947eb7317069119c84493c0285a968834244
Teperi Lee <teperi31@gmail.com>
2019-05-21 16:49:06 +0900

    Updates

---
+++ precache-manifest.d6bc417a253182f9e817fdb01437376e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a60eea66d53de9ceeae152cfb1ecedd6",
+    "url": "/portfolio_weatherWeb/index.html"
+  },
+  {
+    "revision": "dbb6e15df13a2676b081",
+    "url": "/portfolio_weatherWeb/static/css/main.1a52a752.chunk.css"
+  },
+  {
+    "revision": "b20211248595a3641465",
+    "url": "/portfolio_weatherWeb/static/js/2.0c8c7e2a.chunk.js"
+  },
+  {
+    "revision": "dbb6e15df13a2676b081",
+    "url": "/portfolio_weatherWeb/static/js/main.e92a019d.chunk.js"
+  },
+  {
+    "revision": "4fddc696c4fe888cf0c2",
+    "url": "/portfolio_weatherWeb/static/js/runtime~main.7530bf97.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/weatherweb/precache-manifest.6d8ca7a7a9d6c31c4798505398a1ae07.js"
+  "/portfolio_weatherWeb/precache-manifest.d6bc417a253182f9e817fdb01437376e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/weatherweb/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio_weatherWeb/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
